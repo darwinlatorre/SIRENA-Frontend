a8569508ad067403c3dd317c98e0595a6274175f
jlopeztobar <jlopeztobar@unicauca.edu.co>
2023-10-27 02:09:59 -0500

    added CRUD views

--- sirena-app/src/app/pages/pages.module.ts
+++ sirena-app/src/app/pages/pages.module.ts
@@ -3,13 +3,23 @@ import { CommonModule } from '@angular/common';
 import { HomeAdminComponent } from './home-admin/home-admin.component';
 import { HomeUserComponent } from './home-user/home-user.component';
 import { ComponentsModule } from '../components/components.module';
+import { CRUDClassroomComponent } from './crud-classroom/crud-classroom.component';
+import { CRUDScheduleComponent } from './crud-schedule/crud-schedule.component';
+import { CRUDReservationComponent } from './crud-reservation/crud-reservation.component';
+import { CRUDUsersComponent } from './crud-users/crud-users.component';
+import { CRUDReservationUpdateComponent } from './crud-reservation-update/crud-reservation-update.component';
 
 
 
 @NgModule({
   declarations: [
     HomeAdminComponent,
-    HomeUserComponent
+    HomeUserComponent,
+    CRUDClassroomComponent,
+    CRUDScheduleComponent,
+    CRUDReservationComponent,
+    CRUDUsersComponent,
+    CRUDReservationUpdateComponent,
   ],
   imports: [
     CommonModule,
